61189269000eb6d7c94ec386d08dc3ada258e1cc
Adam Dakota <aviternus@gmail.com>
2020-07-24 23:57:01 -0700

    Initial set up of login screen
    Signed-off-by: Adam Dakota <aviternus@gmail.com>

--- Spreader_UI/TD_Test.py
+++ Spreader_UI/TD_Test.py
@@ -21,11 +21,23 @@ class C_Spread:
     def get_TDClient(self):
         # Create a new session, credentials path is optional.
         TDSession = TDClient(
-            client_id='LZGUB1XDODQPG9D95BZ0G7ZV8AMAUVM8',
-            redirect_uri="http://localhost/green_chip",
-            credentials_path="credentials.json"
+            client_id='',
+            redirect_uri="",
+            credentials_path=""
         )
         return TDSession
+
+    def get_symbols(self):
+        path = 'nasdaqlisted.txt'  # ticker file
+        delimiter = '|'
+
+        with open(path, 'r') as nasdaq_list_file:
+            for row in nasdaq_list_file.readlines():
+                row = row.replace('\n', '')  # remove newline characters
+                row_array = row.split(delimiter)
+                self.symbols.append(row_array[0])
+
+        self.symbols.pop(0)  # remove the header row
 
 if __name__ == "__main__":
     c_spread = C_Spread()
